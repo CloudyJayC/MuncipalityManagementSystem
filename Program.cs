8a33584f2d70eb07496d86c84ec738af49b9c424
CloudyJayC <jwcameron3@gmail.com>
2025-03-31 20:29:43 +0200

    Updated Program.cs and added folder and class
    Added Data Folder and a new class called "ApplicationDbContext" and updated Program.cs to connect to the newly created class
    removed 1 line of code (hello world

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MuncipalityManagementSystem.Data; // Namespace for ApplicationDbContext
+
 var builder = WebApplication.CreateBuilder(args);
+
+// This adds the  Database Context (Registering DbContext)
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// This adds MVC services (so Controllers and Views work)
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// This enables MVC Routing (so it can find Controllers)
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapControllerRoute(
+		name: "default",
+		pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
